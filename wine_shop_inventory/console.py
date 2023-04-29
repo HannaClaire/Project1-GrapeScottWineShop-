27937d3683c1a64aec21bc7fd405599556e6817b
Hanna <hannadurham3@gmail.com>
2023-04-29 22:14:02 +0100

    fourth commit with further additions and completed mvp

--- wine_shop_inventory/console.py
+++ wine_shop_inventory/console.py
@@ -11,8 +11,11 @@ import repositories.producer_repository as producer_repo
 producer1 = Producer("FancyPants")
 producer_repo.save(producer1)
 
-wine1 = Wine("Pinot Gris", "White - medium/full-bodied, Notes and flavours: delicate/floral aromatics; round feel, nutty,spicy/pears, melon on palette", 6, 15, 18, producer1.id)  
+wine1 = Wine("Pinot Gris", "White - medium/full-bodied, Notes & flavours: delicate/floral aromatics; round feel, nutty/spicy, pears/melon on palette", 6, 15, 18, producer1.id)  
 wine_repo.save(wine1)
 
-wine2 = Wine("Pinot Noir", "Red - light/medium-bodied, Notes and flavours: red fruits/ spicey aromas, red fruits, clove, hibiscus, umami on palette", 15, 13, 17, producer1.id)
+wine2 = Wine("Pinot Noir", "Red - light/medium-bodied, Notes & flavours: red fruits/spicey aromas, red fruits, clove, hibiscus, umami on palette", 15, 13, 17, producer1.id)
 wine_repo.save(wine2)
+
+wine3 = Wine("Pinot Blanc", "White - medium/full-bodies, Notes & flavours: floral aromas, round feel, peaches/pears/almonds and slight minerality on the palette", 0, 18, 21, producer1.id)
+wine_repo.save(wine3)
